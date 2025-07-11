5f3dd13311ead19df9ca500bb3fbb0c796a1dd8d
daekwon <pranludi@gmail.com>
2025-07-11 15:14:39 +0900

    member web layer

--- src/main/java/com/example/jpashop/domain/Address.java
+++ src/main/java/com/example/jpashop/domain/Address.java
@@ -3,11 +3,13 @@ package com.example.jpashop.domain;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Address {
